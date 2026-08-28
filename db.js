cf221ea568a4f3a655059e11688186402f45c9bd
olawale <eolawale33@gmail.com>
2026-08-28 12:20:18 +0100

    Database error

--- db.js
+++ db.js
@@ -2,7 +2,15 @@ require('dotenv').config();
 const {Pool} = require('pg');
 
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DATABASE_URL || process.env.POSTGRES_URL;
+
+console.log(
+    'DATABASE_HOST:',
+    connectionString
+         ? new URL(connectionString).hostname
+         : 'NO DATABASE URL'
+);
+
 const pool = new Pool({
     connectionString,
    ssl: {
